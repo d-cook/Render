760fd43318cea548786b55e876f0729670c9dd9f
dcook <dcook0819@gmail.com>
2018-08-02 22:40:19 -0700

    Added "curve" (does bezier or quadratic)

--- render.js
+++ render.js
@@ -37,21 +37,34 @@ function renderer(config, width, height) {
     buffer.height = canvas.height = (typeof height === 'number') ? height : canvas.width;
 
     var ops = {
-        line:      function line     (/*..xi,yi..*/) { path(arguments, 0, 0); },
-        poly:      function poly     (/*..xi,yi..*/) { path(arguments, 1, 0); },
-        solidpoly: function solidpoly(/*..xi,yi..*/) { path(arguments, 1, 1); },
-        rect:      function rect     (x, y, w, h) { ctx.strokeRect(xof(x+0.5, w-1), yof(y+0.5, h-1), w-1, h-1); },
-        solidrect: function solidrect(x, y, w, h) { ctx.fillRect  (xof(x,     w  ), yof(y,     h  ), w,   h  ); },
-        clear:     function clearrect(x, y, w, h) { ctx.clearRect (xof(x,     w  ), yof(y,     h  ), w  , h  ); },
+        line:       function line     (/*..xi,yi..*/) { path(arguments, 0, 0); },
+        poly:       function poly     (/*..xi,yi..*/) { path(arguments, 1, 0); },
+        solidpoly:  function solidpoly(/*..xi,yi..*/) { path(arguments, 1, 1); },
+        rect:       function rect     (x, y, w, h) { ctx.strokeRect(xof(x+0.5, w-1), yof(y+0.5, h-1), w-1, h-1); },
+        solidrect:  function solidrect(x, y, w, h) { ctx.fillRect  (xof(x,     w  ), yof(y,     h  ), w,   h  ); },
+        clear:      function clearrect(x, y, w, h) { ctx.clearRect (xof(x,     w  ), yof(y,     h  ), w  , h  ); },
+        curve:      function curve(x1, y1, x2, y2, cx1, cy1, cx2, cy2) {
+                        ctx.moveTo(x1, y1);
+                        if (arguments.length > 6) { ctx.bezierCurveTo(xof(cx1+0.5), yof(cy1+0.5), xof(cx2+0.5), yof(cy2+0.5), xof(x2+0.5), yof(y2+0.5)); }
+                        else /***************/ { ctx.quadraticCurveTo(xof(cx1+0.5), yof(cy1+0.5), /*************************/ xof(x2+0.5), yof(y2+0.5)); }
+                        ctx.stroke();
+                    },
+        solidcurve: function curve(x1, y1, x2, y2, cx1, cy1, cx2, cy2) {
+                        ctx.moveTo(x1, y1);
+                        if (arguments.length > 6) { ctx.bezierCurveTo(xof(cx1), yof(cy1), xof(cx2), yof(cy2), xof(x2), yof(y2)); }
+                        else /***************/ { ctx.quadraticCurveTo(xof(cx1), yof(cy1), /*****************/ xof(x2), yof(y2)); }
+                        ctx.fill();
+                    }
     };
 
     function xof(x, w) { return originX + dx * (dx > 0 ? x : x + (w||0)); }
     function yof(y, h) { return originY + dy * (dy > 0 ? y : y + (h||0)); }
 
     function path(args, close, fill) {
-        ctx.moveTo(xof(args[0]+0.5), yof(args[1]+0.5));
+        var d = (fill ? 0 : 0.5);
+        ctx.moveTo(xof(args[0]+d), yof(args[1]+d));
         for(var i = 2; i < args.length - 1; i += 2) {
-            ctx.lineTo(xof(args[i]+0.5), yof(args[i+1]+0.5));
+            ctx.lineTo(xof(args[i]+d), yof(args[i+1]+d));
         }
         if (close) { ctx.closePath(); }
         ctx[fill ? 'fill' : 'stroke']();
